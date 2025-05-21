44c5046e5842d696a65ef31011092e6fa356c3a5
siddharth7020 <sidh1583@gmail.com>
2025-05-21 10:53:53 +0530

    Add start script and configure server host and port in Vite

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,8 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: '0.0.0.0',
+    port: process.env.PORT ? parseInt(process.env.PORT) : 5173,
     proxy: {
       '/api': {
         target: 'https://lead-backend-o70f.onrender.com/',
